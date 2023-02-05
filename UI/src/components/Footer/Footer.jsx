631cdf86dbebcf6b25e96b7d683077d45f21c8f1
stebin cleetus <stebincleetus@gmail.com>
2023-02-05 22:49:30 +0530

    all files Added

---
+++ UI/src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+     <footer class="bg-light text-center text-lg-start">
+  <div class="text-center p-3">
+    <a class="text-dark" >Stebin Cleetus assignment</a>
+  </div>
+ 
+</footer>
+    </div>
+  );
+}
+
+export default Footer;
